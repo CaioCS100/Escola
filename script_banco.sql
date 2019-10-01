c77d834bc63977f1987252ca20673022059c6df2
Caio <caio.cesar.s100@gmail.com>
2019-10-01 12:39:51 -0300

    Definindo o banco de dados e colocando css na tela de login

---
+++ script_banco.sql
@@ -0,0 +1,96 @@
+﻿create table categorias(
+
+id serial primary key,
+descricao character varying (30),
+ultima_modificacao timestamp default now()
+);
+
+create table dados_matricula(
+
+id serial primary key,
+matricula integer,
+senha varchar(100),
+id_categoria integer not null,
+ultima_modificacao timestamp default now(),
+foreign key(id_categoria) references categorias(id)
+);
+
+create table alunos(
+
+id serial primary key,
+nome character varying(100),
+idade integer,
+cep character varying(10),
+endereco character varying(200),
+cidade character varying(100),
+id_matricula integer,
+ultima_modificacao timestamp default now(),
+foreign key(id_matricula) references dados_matricula(id)
+);
+
+create table professores(
+
+id serial primary key,
+nome character varying(100),
+idade integer,
+cep character varying(10),
+endereco character varying(200),
+cidade character varying(100),
+id_matricula integer,
+ultima_modificacao timestamp default now(),
+foreign key(id_matricula) references dados_matricula(id)
+);
+
+create table funcionarios(
+
+id serial primary key,
+matricula integer,
+nome character varying(100),
+idade integer,
+cep character varying(10),
+endereco character varying(200),
+cidade character varying(100),
+id_matricula integer,
+ultima_modificacao timestamp default now(),
+foreign key(id_matricula) references dados_matricula(id)
+);
+
+create table materias(
+
+id serial primary key,
+nome character varying (100),
+ultima_modificacao timestamp default now()
+);
+
+create table cursos(
+
+id serial primary key,
+id_materia integer not null,
+id_professor integer not null,
+ultima_modificacao timestamp default now(),
+foreign key(id_materia) references materias(id),
+foreign key(id_professor) references professores(id)
+);
+
+create table turmas(
+
+id serial primary key,
+id_curso integer not null,
+id_aluno integer not null,
+ultima_modificacao timestamp default now(),
+foreign key(id_curso) references cursos(id),
+foreign key(id_aluno) references alunos(id)
+);
+
+INSERT INTO categorias(descricao)
+    VALUES ('Aluno'), ('Professor'), ('Funcionario');
+
+INSERT INTO dados_matricula(matricula, senha, id_categoria)
+    VALUES (101010, '123', 3);
+
+INSERT INTO funcionarios(nome, idade, cep, endereco, cidade, id_matricula)
+    VALUES ('Adm', 50, '77.017-280', 'Quadra 903 Sul Alameda 15', 'Palmas', 1);
+
+
+
+
